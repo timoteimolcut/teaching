efb77eecefbee986e1f9f20a599c4495cde2a54c
Timotei Molcut <timoteim98@gmail.com>
2026-02-21 18:36:48 +0200

    Inheritance - Tim
    Signed-off-by: Timotei Molcut <timoteim98@gmail.com>

--- oop.py
+++ oop.py
@@ -1,12 +1,13 @@
 import time as t
 from time import time
 
+
 class BankAccount:
     """predefined, but overwriten"""
-    def __init__(self, owner_to_be_added="Alice", balance_to_be_added=123, deposit_timestamps=[]):# constructor
+    def __init__(self, owner_to_be_added="Alice", balance_to_be_added=123):# constructor
         self.owner = owner_to_be_added
         self.balance = balance_to_be_added
-        self.deposit_timestamps = deposit_timestamps # property
+        self.deposit_timestamps = [] # property
     
     def __str__(self):# to string
         times = ""
@@ -28,32 +29,50 @@ class BankAccount:
             print("Insuficient balance!")
         else:
             self.balance -= amount
-    
-    
-account1 = BankAccount("Bob", 200)
-print(account1)
-account1.deposit()
-print(account1)
-account1.deposit()
-print(account1)
-account1.deposit()
-print(account1)
-account1.withdraw()
-print(account1)
-account1.withdraw(900)
-print(account1)
 
-print("________")
 
-account2 = BankAccount()
-print(account2)
-account1.deposit()
-print(account1)
-take_amount = 2000
-BankAccount.withdraw(account2, take_amount)
-print(account2)
-print(type(account2))
+# inheritance
+class SavingsAccount(BankAccount):
+    def __init__(self, owner="Alice", balance=123, interest_rate=3.0):
+        super().__init__(owner, balance)
+        self.interest_rate = interest_rate
 
+    def apply_interest(self):
+        self.balance += self.balance * self.interest_rate/100
+
+dummy = "inheritance"
+
+if dummy != "inheritance":
+    account1 = BankAccount("Bob", 200)
+    print(account1)
+    account1.deposit()
+    print(account1)
+    account1.deposit()
+    print(account1)
+    account1.deposit()
+    print(account1)
+    account1.withdraw()
+    print(account1)
+    account1.withdraw(900)
+    print(account1)
+
+    print("________")
+
+    account2 = BankAccount()
+    print(account2)
+    account1.deposit()
+    print(account1)
+    take_amount = 2000
+    BankAccount.withdraw(account2, take_amount)
+    print(account2)
+    print(type(account2))
+else:
+    save_account = SavingsAccount()
+    print(save_account)
+    n = 5
+    for i in range(n):
+        save_account.apply_interest()
+        print(save_account)
 
 
 
