e56ccb776f00d33c3fa5ad3e9bd418befd4a2700
wdigena-coder <wdigena@gmail.com>
2026-03-22 13:45:24 +0100

    creating package
    Signed-off-by: wdigena-coder <wdigena@gmail.com>

---
+++ project/main.py
@@ -0,0 +1,32 @@
+"""
+importing area
+"""
+
+# import utils
+
+# import utils.math_utils
+from utils import math_utils
+    # These two are interchangeable
+
+from utils.math_utils import PI 
+
+"""definition area: function/methods, variables
+"""
+
+def fun():
+    pass
+
+def fun2():
+    pass
+
+"""calling area - for main script
+"""
+
+if __name__ == "__main__":
+    print("Entering main script.")
+    print(PI)
+    print(f"this is {__name__}")
+    fun()
+    fun2()
+    print(math_utils.add(2, 2))
+    print(math_utils.multiply(3, 2))
---
+++ project/utils/math_utils.py
@@ -0,0 +1,15 @@
+
+PI = 3.1415
+
+def add(a, b):
+    return a+b
+
+def multiply(a, b):
+    return a*b
+
+if __name__ == "__main__":
+    print("Entering math.utils script.")
+    print(add(2, 2))
+else:
+    print(f"module {__name__} imported")
+    
