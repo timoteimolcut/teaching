336480b906584ccf32c38322e350af2c3082a548
Timotei Molcut <timoteim98@gmail.com>
2026-03-28 17:02:44 +0200

    Added .gitignore file + package management
    Signed-off-by: Timotei Molcut <timoteim98@gmail.com>

---
+++ project/utils/__init__.py
@@ -0,0 +1,11 @@
+
+
+"""
+                         package
+    inside one package:  multiple modules (at least one)
+    inside one module:   multiple classe (at least one) or just defition code
+    inside one class:    just defition code
+
+"""
+
+from .math_utils import PI, add, multiply
